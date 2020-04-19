7ab0d9173f4142d470e57088d563c4d33c82883b
Chris <chris.demetriad@thecommercepartnership.com>
2020-04-19 18:36:51 +0100

    Showing some dummy data on the listing page

--- src/Ad/Listing.js
+++ src/Ad/Listing.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { dummyAds } from "./../Shared/Data";
+// console.log(Data);
 
 const Listing = () => {
   return (
     <>
       <h1>Listing</h1>
+      {dummyAds.map((ad) => (
+        <div>
+          <p> {ad.name} </p>
+          <p> {ad.bodyText} </p>
+          <p> {ad.prodId} </p>
+          <p> {ad.email} </p>
+          <p> {ad.town} </p>
+          <p> {ad.adTitle} </p>
+        </div>
+      ))}
     </>
   );
 };

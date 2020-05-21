42588af28e0c89fc38f878647a4ee9ad315e5f2d
Chris <chris.demetriad@thecommercepartnership.com>
2020-05-21 16:12:54 +0100

    Pushing up the build and modified .gitignore file

---
+++ build/static/js/14.e0f42556.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbsc-proj"]=this["webpackJsonpbsc-proj"]||[]).push([[14],{72:function(e,n,t){"use strict";t.r(n);var s=t(0),c=t.n(s);n.default=function(){return c.a.createElement("div",null,c.a.createElement("h2",null,"Cookies"))}}}]);
+//# sourceMappingURL=14.e0f42556.chunk.js.map

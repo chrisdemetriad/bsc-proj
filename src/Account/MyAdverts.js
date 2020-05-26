79e1deb3fb8fa0d5f4dde892819966601f92fcb7
Chris <chris.demetriad@thecommercepartnership.com>
2020-05-26 12:12:12 +0100

    Finishing My adverts section

--- src/Account/MyAdverts.js
+++ src/Account/MyAdverts.js
@@ -43,43 +43,91 @@ const MyAdverts = (props) => {
 			});
 	}
 
+	const listing = css`
+		flex: 0 0 24%;
+		border: 1px solid #eee;
+		padding: 0;
+		max-width: 24%;
+		margin-bottom: 15px;
+		background: white;
+		border-radius: 0px 0px 2px 2px;
+		box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.13);
+		transition: box-shadow 0.2s;
+		:hover {
+			box-shadow: 0 6px 12px 0 rgba(0, 0, 0, 0.15);
+			svg {
+				color: black;
+			}
+		}
+	`;
+	const actions = css`
+		border-top: 6px solid indianred;
+		padding: 1rem;
+		display: flex;
+		justify-content: space-between;
+	`;
+	const title = css`
+		text-align: center;
+		padding: 1rem;
+		border-bottom: 6px solid indianred;
+	`;
+
 	const image = css`
-		max-height: 100px;
-		width: auto;
+		display: flex;
+		height: 200px;
+		align-items: center;
+		justify-content: center;
+
+		background: rgb(34, 193, 195);
+		background: radial-gradient(circle, rgba(34, 193, 195, 1) 0%, rgba(169, 62, 136, 1) 0%, rgba(193, 65, 169, 1) 19%, rgba(198, 63, 186, 1) 29%, rgba(109, 85, 197, 1) 74%, rgba(76, 93, 201, 1) 79%, rgba(128, 190, 131, 1) 100%, rgba(45, 168, 253, 1) 100%);
+		img {
+			max-height: 200px;
+		}
 	`;
 
 	return (
 		<MainLayout>
 			<h2>My adverts</h2>
-			<div className="row">
+			<div className="row no-gutters d-flex justify-content-between">
 				{advert.length > 0 ? (
 					advert.map((ad) => (
-						<div classNames="col-md-4" key={ad.docId}>
+						<div classNames="col-md-3" key={ad.docId} css={listing}>
+							<div css={title}>
+								<Link to={"/advert/" + ad.docId} data="nice">
+									{ad.title} - {ad.price}
+								</Link>
+							</div>
 							{ad.file.length > 0 ? (
 								ad.file.map((item, index) => {
-									return (
-										<span key={index}>
-											<img src={item.url} width="300" css={image} />
-										</span>
-									);
+									// @todo only showing one photo for now
+									if (index === 1) {
+										return;
+									} else {
+										return (
+											<Link to={"/advert/" + ad.docId} key={ad.docId}>
+												<div key={index} css={image}>
+													{<img src={item.url} alt={ad.title} />}
+												</div>
+											</Link>
+										);
+									}
 								})
 							) : (
-								<span>No images</span>
+								<div>No images</div>
 							)}
-							<Link
-								onClick={() => {
-									deleteAdvert(ad.docId);
-								}}
-								title="Delete advert"
-							>
-								<AiOutlineDelete />
-							</Link>
-							<Link to={"/advert/" + ad.docId} data="nice">
-								{ad.title} - {ad.price}
-							</Link>
-							<Link to={"/edit/" + ad.docId} data="edit" title="Edit advert">
-								<AiOutlineEdit />
-							</Link>
+							<div css={actions}>
+								<Link
+									onClick={() => {
+										deleteAdvert(ad.docId);
+									}}
+									title="Delete advert"
+								>
+									<AiOutlineDelete />
+								</Link>
+								<Link to={"/edit/" + ad.docId} data="edit" title="Edit advert">
+									<AiOutlineEdit />
+								</Link>
+							</div>
 						</div>
 					))
 				) : (

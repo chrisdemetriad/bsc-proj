7c8009ef173c2f0529b826369ea770724394f418
Chris <chris.demetriad@thecommercepartnership.com>
2020-06-01 18:19:52 +0100

    Header text change

--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -61,7 +61,7 @@ const Header = () => {
 					<div>
 						<Link to="/">
 							{/* <h1 css={logo}>5th</h1> */}
-							<img src={logo} alt="Logo" width="80" title="7th" />
+							<img src={logo} alt="Logo" width="80" title="8th" />
 						</Link>
 					</div>
 				</div>

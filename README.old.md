2924a455a92cd3cf24d05a8dbb6bf28488b82734
Chris <chris.demetriad@thecommercepartnership.com>
2020-06-01 15:18:13 +0100

    Updates

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# bsc-proj
-BSc Final Year Project (Project Information Systems and Management Type 4) (BUCI027S6) 
---
+++ precache-manifest.0fe7c84d28b5d10b00692de5f79c1354.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5953cd02f88cf4efca1b6ff33e9c7d",
+    "url": "/bbk-proj/index.html"
+  },
+  {
+    "revision": "34ec592522e2a62310ea",
+    "url": "/bbk-proj/static/css/main.cb752bdc.chunk.css"
+  },
+  {
+    "revision": "53fefc1416dd3b26cc29",
+    "url": "/bbk-proj/static/js/0.deaeca2e.chunk.js"
+  },
+  {
+    "revision": "0708c1a22a5a673705b7",
+    "url": "/bbk-proj/static/js/1.5f766f02.chunk.js"
+  },
+  {
+    "revision": "5869b488b5e260f738553167b3122b4a",
+    "url": "/bbk-proj/static/js/1.5f766f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a3ab8cce41c6d22841",
+    "url": "/bbk-proj/static/js/10.60827642.chunk.js"
+  },
+  {
+    "revision": "7ccc32dbd7b3894ac4f3",
+    "url": "/bbk-proj/static/js/11.23bb5fa5.chunk.js"
+  },
+  {
+    "revision": "ccf4cd09be485e32c47c",
+    "url": "/bbk-proj/static/js/12.0294d711.chunk.js"
+  },
+  {
+    "revision": "633b123fb9680c8c83f5",
+    "url": "/bbk-proj/static/js/13.8505d774.chunk.js"
+  },
+  {
+    "revision": "6565cfcc5557d858214b",
+    "url": "/bbk-proj/static/js/14.48c4c11b.chunk.js"
+  },
+  {
+    "revision": "6a636bbfe4b2d42d6347",
+    "url": "/bbk-proj/static/js/15.cbdc7836.chunk.js"
+  },
+  {
+    "revision": "7753bc94a4f17fd6efd7",
+    "url": "/bbk-proj/static/js/16.ce1f1ea0.chunk.js"
+  },
+  {
+    "revision": "dc58fdb55f7ad700d328",
+    "url": "/bbk-proj/static/js/17.dc7989f5.chunk.js"
+  },
+  {
+    "revision": "4fd4995ad64384e1a344",
+    "url": "/bbk-proj/static/js/4.fa806f66.chunk.js"
+  },
+  {
+    "revision": "2197e867d978872afb824139f7f0a37a",
+    "url": "/bbk-proj/static/js/4.fa806f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05cba5637a96f909c6e3",
+    "url": "/bbk-proj/static/js/5.c815acec.chunk.js"
+  },
+  {
+    "revision": "4adf97848866579c1afe",
+    "url": "/bbk-proj/static/js/6.7f904640.chunk.js"
+  },
+  {
+    "revision": "8c39f2496d2755063250",
+    "url": "/bbk-proj/static/js/7.3bc40fc4.chunk.js"
+  },
+  {
+    "revision": "cf38e5e27e7649602e61",
+    "url": "/bbk-proj/static/js/8.3506fabf.chunk.js"
+  },
+  {
+    "revision": "b8fcafd58d4321d672ae",
+    "url": "/bbk-proj/static/js/9.c9663c8d.chunk.js"
+  },
+  {
+    "revision": "34ec592522e2a62310ea",
+    "url": "/bbk-proj/static/js/main.339de32d.chunk.js"
+  },
+  {
+    "revision": "166d684c9fde4da23b12",
+    "url": "/bbk-proj/static/js/runtime-main.b05de799.js"
+  },
+  {
+    "revision": "fa29ff4d384a2868ba86cc4f897ffe87",
+    "url": "/bbk-proj/static/media/404.fa29ff4d.jpg"
+  },
+  {
+    "revision": "0a7742378df01f3c4fc2df2d53159ea9",
+    "url": "/bbk-proj/static/media/geomanist-bold-webfont.0a774237.ttf"
+  },
+  {
+    "revision": "710cedaf67c4c630429aa1b827f56fea",
+    "url": "/bbk-proj/static/media/geomanist-medium-webfont.710cedaf.ttf"
+  },
+  {
+    "revision": "0f2de97bae47a1d1f00ab33422220f8c",
+    "url": "/bbk-proj/static/media/geomanist-regular-webfont.0f2de97b.svg"
+  },
+  {
+    "revision": "15172c4485d205a9b749ddff016d8dd3",
+    "url": "/bbk-proj/static/media/geomanist-regular-webfont.15172c44.eot"
+  },
+  {
+    "revision": "7da201004f3c567bae2df158acb0b639",
+    "url": "/bbk-proj/static/media/geomanist-regular-webfont.7da20100.ttf"
+  },
+  {
+    "revision": "a35649b1d4c9738de84be469ebdf3b2e",
+    "url": "/bbk-proj/static/media/geomanist-regular-webfont.a35649b1.woff2"
+  },
+  {
+    "revision": "deb2e275f84cb3a34faaccd5f0daa4f7",
+    "url": "/bbk-proj/static/media/geomanist-regular-webfont.deb2e275.woff"
+  },
+  {
+    "revision": "9bd2252e9cdc3ebea925a996b5e64566",
+    "url": "/bbk-proj/static/media/geomanist-ultra-webfont.9bd2252e.ttf"
+  },
+  {
+    "revision": "9bc6e05d567e3eb9b6f28159dd97af51",
+    "url": "/bbk-proj/static/media/logo.9bc6e05d.png"
+  },
+  {
+    "revision": "aafa6646bb4c56ecb471a18a51c519c5",
+    "url": "/bbk-proj/static/media/promo.aafa6646.jpg"
+  }
+]);

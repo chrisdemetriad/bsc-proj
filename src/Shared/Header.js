539b30b8dfee7a54ba8e4e87662f126366b00624
Chris <chris.demetriad@thecommercepartnership.com>
2020-06-01 18:11:22 +0100

    Header text change to trigger autodeployment

--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -61,7 +61,7 @@ const Header = () => {
 					<div>
 						<Link to="/">
 							{/* <h1 css={logo}>5th</h1> */}
-							<img src={logo} alt="Logo" width="80" title="3rd" />
+							<img src={logo} alt="Logo" width="80" title="4th" />
 						</Link>
 					</div>
 				</div>

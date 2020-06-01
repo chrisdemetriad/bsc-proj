e62c8608bfe771e2a39bb175fb51e1b889ec1ec6
Chris <chris.demetriad@thecommercepartnership.com>
2020-06-01 17:12:27 +0100

    Slight change to test the deploy

--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -61,7 +61,7 @@ const Header = () => {
 					<div>
 						<Link to="/">
 							{/* <h1 css={logo}>5th</h1> */}
-							<img src={logo} alt="Logo" width="80" />
+							<img src={logo} alt="Logo" width="80" title="5th" />
 						</Link>
 					</div>
 				</div>

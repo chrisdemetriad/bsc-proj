38dc1d08c257a9eeb54c362fed5283a7da816290
Chris <chris.demetriad@thecommercepartnership.com>
2020-06-01 17:15:13 +0100

    Fixes

---
+++ build/precache-manifest.1a486a78a4614c097f3b5253eb740929.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88181f7759a90b1829e35ee436187ba2",
+    "url": "/bsc-proj/index.html"
+  },
+  {
+    "revision": "04caac576e70d67cbe93",
+    "url": "/bsc-proj/static/css/main.2c770c37.chunk.css"
+  },
+  {
+    "revision": "a30e293cb34370e74ab2",
+    "url": "/bsc-proj/static/js/0.729d42bc.chunk.js"
+  },
+  {
+    "revision": "1ff46bc36f84da9d864d",
+    "url": "/bsc-proj/static/js/1.2780d20b.chunk.js"
+  },
+  {
+    "revision": "5869b488b5e260f738553167b3122b4a",
+    "url": "/bsc-proj/static/js/1.2780d20b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8721e0b86fa31d0c8b96",
+    "url": "/bsc-proj/static/js/10.669be41a.chunk.js"
+  },
+  {
+    "revision": "85323720ec7b7dd7df1a",
+    "url": "/bsc-proj/static/js/11.34ebb1ed.chunk.js"
+  },
+  {
+    "revision": "7a3da16113e41cce2198",
+    "url": "/bsc-proj/static/js/12.5078e359.chunk.js"
+  },
+  {
+    "revision": "8ebf35aedcb97acc7849",
+    "url": "/bsc-proj/static/js/13.bbb7508b.chunk.js"
+  },
+  {
+    "revision": "8b20f3e9c40c0a6b21cc",
+    "url": "/bsc-proj/static/js/14.8c28359c.chunk.js"
+  },
+  {
+    "revision": "11d3df89a06aedb27350",
+    "url": "/bsc-proj/static/js/15.cc662a9a.chunk.js"
+  },
+  {
+    "revision": "4c1a81b2c958d551954a",
+    "url": "/bsc-proj/static/js/16.0b7ccf80.chunk.js"
+  },
+  {
+    "revision": "94ac4297d58541d751b4",
+    "url": "/bsc-proj/static/js/17.79233e7d.chunk.js"
+  },
+  {
+    "revision": "5d4bc6c1f126f95f1837",
+    "url": "/bsc-proj/static/js/4.884ecf4d.chunk.js"
+  },
+  {
+    "revision": "2197e867d978872afb824139f7f0a37a",
+    "url": "/bsc-proj/static/js/4.884ecf4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e707c42af3e00b52fdbf",
+    "url": "/bsc-proj/static/js/5.6a8ea060.chunk.js"
+  },
+  {
+    "revision": "24db992814d3ad81b8dd",
+    "url": "/bsc-proj/static/js/6.e9f564f4.chunk.js"
+  },
+  {
+    "revision": "1f31e264704603cb6dbc",
+    "url": "/bsc-proj/static/js/7.58a4f09c.chunk.js"
+  },
+  {
+    "revision": "1a2e6c1c6d227ac81f2c",
+    "url": "/bsc-proj/static/js/8.f98090df.chunk.js"
+  },
+  {
+    "revision": "03d4e97df163fe780998",
+    "url": "/bsc-proj/static/js/9.01a145e4.chunk.js"
+  },
+  {
+    "revision": "04caac576e70d67cbe93",
+    "url": "/bsc-proj/static/js/main.b5c36815.chunk.js"
+  },
+  {
+    "revision": "acab6aa3cb6ec84b52ed",
+    "url": "/bsc-proj/static/js/runtime-main.5612b5db.js"
+  },
+  {
+    "revision": "fa29ff4d384a2868ba86cc4f897ffe87",
+    "url": "/bsc-proj/static/media/404.fa29ff4d.jpg"
+  },
+  {
+    "revision": "0a7742378df01f3c4fc2df2d53159ea9",
+    "url": "/bsc-proj/static/media/geomanist-bold-webfont.0a774237.ttf"
+  },
+  {
+    "revision": "710cedaf67c4c630429aa1b827f56fea",
+    "url": "/bsc-proj/static/media/geomanist-medium-webfont.710cedaf.ttf"
+  },
+  {
+    "revision": "0f2de97bae47a1d1f00ab33422220f8c",
+    "url": "/bsc-proj/static/media/geomanist-regular-webfont.0f2de97b.svg"
+  },
+  {
+    "revision": "15172c4485d205a9b749ddff016d8dd3",
+    "url": "/bsc-proj/static/media/geomanist-regular-webfont.15172c44.eot"
+  },
+  {
+    "revision": "7da201004f3c567bae2df158acb0b639",
+    "url": "/bsc-proj/static/media/geomanist-regular-webfont.7da20100.ttf"
+  },
+  {
+    "revision": "a35649b1d4c9738de84be469ebdf3b2e",
+    "url": "/bsc-proj/static/media/geomanist-regular-webfont.a35649b1.woff2"
+  },
+  {
+    "revision": "deb2e275f84cb3a34faaccd5f0daa4f7",
+    "url": "/bsc-proj/static/media/geomanist-regular-webfont.deb2e275.woff"
+  },
+  {
+    "revision": "9bd2252e9cdc3ebea925a996b5e64566",
+    "url": "/bsc-proj/static/media/geomanist-ultra-webfont.9bd2252e.ttf"
+  },
+  {
+    "revision": "9bc6e05d567e3eb9b6f28159dd97af51",
+    "url": "/bsc-proj/static/media/logo.9bc6e05d.png"
+  },
+  {
+    "revision": "aafa6646bb4c56ecb471a18a51c519c5",
+    "url": "/bsc-proj/static/media/promo.aafa6646.jpg"
+  }
+]);
